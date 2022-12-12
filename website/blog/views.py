ba5d6e99d692c03aefa1a07ba99b67413a35209f
Stepan Kolodkin <zorokon@yandex.ru>
2022-12-12 22:45:52 +0300

    add pagination

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,24 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 
 
-@login_required  # ограничение доступа к профилям для незарегистрированных пользователей
-def home(request):
-    context = {
-        'posts': Post.objects.all().order_by('-date_posted')
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
 
 
-# class PostListView(ListView):
-#     model = Post
-#     template_name = 'blog/home.html'
-#     context_object_name = 'posts'
-#     ordering = ['-date_posted']
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
